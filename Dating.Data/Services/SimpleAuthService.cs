8f7833ceb02a851737f1c356811fe9b86adb2bee
danie <daniel2003@outlook.dk>
2024-02-29 10:51:26 +0100

    pølse

--- Dating.Data/Services/SimpleAuthService.cs
+++ Dating.Data/Services/SimpleAuthService.cs
@@ -7,10 +7,24 @@ using System.Threading.Tasks;
 
 namespace Dating.Data.Services
 {
+    // Definerer en klasse til at håndtere autentificeringsservice.
     public class SimpleAuthService
     {
-
+        // En egenskab, der angiver, om brugeren er logget ind eller ej.
+        // Standardværdien er sat til false, hvilket betyder, at brugeren ikke er logget ind ved start.
         public bool IsLoggedIn { get; set; } = false;
+
+        // En egenskab til at holde information om den nuværende bruger.
+        // Typen 'AccountProfile' formodes at være en brugerdefineret klasse, der indeholder brugerens detaljer.
         public AccountProfile CurrentUser { get; set; }
+        public void Logout()
+        {
+            // Logik for at logge brugeren ud
+            CurrentUser = null;
+            IsLoggedIn = false;
+        }
     }
 }
+
+
+
